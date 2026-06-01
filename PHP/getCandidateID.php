41b7f7dc35f83f1e4e61b6b55929bc2b2ac8cb56
fikiri-malibiche <fikirimalibiche875@gmail.com>
2026-06-01 19:48:47 +0300

    my second commit

--- PHP/getCandidateID.php
+++ PHP/getCandidateID.php
@@ -2,5 +2,6 @@
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $name = isset($_POST['samia a. hassan']) ? $_POST['samia a. hassan'] : "";
     $name =isset($_POST['tundu a. lissu']) ? $_POST['tundu a. lissu'] : ""  ;
-
+    
 }
+?>
